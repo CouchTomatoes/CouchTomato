d4d81f0e3425155e5adacaceb6413109e8ff9924
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-10-01 14:37:57 -0400

    Updated model

--- couchtomato/model.py
+++ couchtomato/model.py
@@ -2,94 +2,29 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.schema import ThreadLocalMetaData
 from elixir import *
-engine = None
-session = scoped_session(sessionmaker(autoflush=True, transactional=True))
-metadata
-class Language(Entity):
-    name = OneToMany("LanguageName")
-class LanguageName(Entity):
-    translation_id = Field(Integer, primary_key=True)
-    language = ManyToOne('Language', primary_key=True)
-    text = Field(Unicode(20), unique=True)
-class Provider(Entity):
-    url = Field(String(20), unique=True)
-class ExternalMovieId(Entity):
-    provider = ManyToOne('Provider')
-    value = Field(Unicode(40))
-    movie = ManyToOne('Movie')
-    # such as IMDB's, maybe some use text -> long
-class MovieTitle(Entity):
-    language = ManyToOne('Language')
-    movie = ManyToOne('Movie')
-    title = Field(UnicodeText)
-    def __init__(self, title, movie=None):
-        self.title = unicode(title)
-        self.movie = movie
-class Movie(Entity):
-    titles = OneToMany('MovieTitle', lazy=False)
-    plot = Field(UnicodeText)
-    year = Field(Integer)
-    tagline = Field(UnicodeText(255))
-    genres = ManyToMany('Genre')
-    identifiers = OneToMany('ExternalMovieId')
-    characters = ManyToMany('Character')
-    def __init__(self, title, year):
-        try:
-            title = unicode(title)
-            self.titles.append(MovieTitle(title))
-        except TypeError:
-            pass
-class Character(Entity):
-    names = OneToMany('CharacterName', required=True)
-    movies = ManyToMany('Movie')
-class CharacterName(Entity):
-    language = ManyToOne('Language')
-    character = ManyToOne('Character')
-class AssociatedPeople(Entity):
-    directors = ManyToMany('Person')
-    screenwriters = ManyToMany('Person')
-    actors = ManyToMany('Person')
-class AspectRatio(Entity):
-    name = Field(Unicode(20), required=True, unique=True)
-    ratio = Field(Float)
-    movies = ManyToMany('Movie')
-    # The Dark Knight has multiple...
-    
-class Specification(Entity):
-    language = ManyToOne('Language')
-    color = Field(Boolean)
-class Genre(Entity):
-    name = Field(UnicodeText(255))
-class Person(Entity):
-    name = Field(UnicodeText(50))
-    birthday = Field(Date)
-class Rating(Entity):
-    name = Field(UnicodeText(40))
-    position = Field(Float)
-    movies = ManyToMany('Movie')
-    def __cmp__(self, other):
-        if self.position < other.position:
-            return -1
-        elif self.primary_key > other.position:
-            return 1
-        else:
-            return 0
-class AgeRating(Entity):
-    name = Field(UnicodeText(40))
-    position = Field(Float)
-    movies = ManyToMany('Movie')
-    def __cmp__(self, other):
-        if self.position < other.position:
-            return -1
-        elif self.primary_key > other.position:
-            return 1
-        else:
-            return 0
-def doctest(engine=None):
-    """Test
-    >>> from couchtomato import model
-    >>> model.metadata.bind = "sqlite:///:memory:"
-    >>> model.setup_all()
-    >>> model.setup_all()
-    """
-    pass
+# We would like to be able to create this schema in a specific database at
+# will, so we can test it easily.
+# Make elixir not bind to any session to make this possible.
+#
+# http://elixir.ematia.de/trac/wiki/Recipes/MultipleDatabasesOneMetadata
+__session__ = None
+class Resource(Entity):
+    """Represents a resource of movies.  This recources can be online or
+    offline."""
+    name = Field(UnicodeString(255))
+    path = Field(UnicodeString(255))
+class Release(Entity):
+    """Logically groups all files that belong to a certain release, such as
+    parts of a movie, subtitles, nfo, trailers etc."""
+    files = OneToMany('File')
+class File(Entity):
+    """File that belongs to a release."""
+    path = Field(UnicodeString(255))
+    release = ManyToOne('Release')
+    # Let's remember the size so we know about offline media.
+    size = Field(Integer)
+    type = ManyToOne('FileType')
+class FileType(Entity):
+    """Types could be trailer, subtitle, movie, partial movie etc."""
+    name = Field(UnicodeString(255))
+    files = OneToMany('File')
