8a47e5db074a6c9283c1f64bd35b42eb6b5cfc8d
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-10-01 14:30:26 -0400

    cli code updated with flask depedencies

--- couchtomato/cli.py
+++ couchtomato/cli.py
@@ -1,5 +1,10 @@
+from flask import Flask
 import argparse
 
 def cmd_couchtomato():
-    print ("Couchtomato.")
-    pass
+    app = Flask(__name__)
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+    if __name__ == "__main__":
+        app.run(debug = True)
