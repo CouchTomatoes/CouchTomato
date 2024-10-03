11191b0a7eb044a93f121196d4e6f423eaf1e09b
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-10-03 16:36:23 -0400

    Authentication

--- couchtomato/__init__.py
+++ couchtomato/__init__.py
@@ -1,5 +1,6 @@
+from couchtomato.core.auth import requires_auth
 from couchtomato.core.logger import CPLog
-from flask import Flask
+from flask.app import Flask
 # from flask.module import Module
 from flask.helpers import url_for
 from flask.templating import render_template
@@ -10,6 +11,7 @@ log = CPLog(__name__)
 
 # web = Module(__name__, 'web')
 # @web.route('/')
+@requires_auth
 @app.route('/')
 def index():
     return render_template('index.html')
