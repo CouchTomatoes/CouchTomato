00609802251d462551b69ba29b08204759fd4309
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-10-04 20:17:57 -0400

    Javascript basics

--- couchtomato/__init__.py
+++ couchtomato/__init__.py
@@ -1,9 +1,11 @@
 from couchtomato.core.auth import requires_auth
 from couchtomato.core.logger import CPLog
 from flask import Flask
+from flask.globals import request
 # from flask.module import Module
 from flask.helpers import url_for
 from flask.templating import render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 log = CPLog(__name__)
@@ -15,3 +17,9 @@ log = CPLog(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    index_url = url_for('web.index')
+    url = request.path[len(index_url):]
+    return redirect(index_url + '#' + url)
